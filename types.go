13aa0738c46028c54b2687e3f478b8018f8a8fbf
dmkjfs <dmkjfs@gmail.com>
2025-10-17 15:39:48 +0300

    add linked list of components

--- types.go
+++ types.go
@@ -1,6 +1,16 @@
 package statloc
 
 type (
+    component struct {
+        Title string
+        Prev  *component
+    }
+
+    componentSet struct {
+        Tail     *component
+        Elements map[string]struct{}
+    }
+
     TableItem struct {
         LOC   uint64
         Files uint64
@@ -14,6 +24,23 @@ type (
         Total      TableItem
     }
 )
+
+func (s *componentSet) Add(title string) *component {
+    s.Elements[title] = struct{}{}
+    newComponent := &component{Title: title, Prev: s.Tail}
+    s.Tail = newComponent
+    return newComponent
+}
+
+func (s *componentSet) Pop() {
+    delete(s.Elements, s.Tail.Title)
+    s.Tail = s.Tail.Prev
+}
+
+func (s *componentSet) In(title string) bool {
+    _, exists := s.Elements[title]
+    return exists
+}
 
 func (t *TableItem) Append(LOC uint64, files uint64) {
 	t.LOC += LOC
