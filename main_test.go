3f24419e5363c3f9718cef2724af4c8239dd6156
dmkjfs <dmkjfs@gmail.com>
2025-10-17 22:50:26 +0300

    improve tests

--- main_test.go
+++ main_test.go
@@ -12,35 +12,35 @@ import (
 	"github.com/statloc/core/internal/mapping"
 )
 
-type ServiceSuite struct {
+type MainSuite struct {
     suite.Suite
-    results map[string]map[string]uint64
+    results core.Statistics
 }
 
-func (s *ServiceSuite) SetupSuite() {
+func (s *MainSuite) SetupSuite() {
     rawResults, _ := os.ReadFile(filepath.Join("testdata", "results.json"))
-    s.results = mapping.LoadJSON[map[string]map[string]uint64](string(rawResults))
+    s.results = mapping.LoadJSON[core.Statistics](string(rawResults))
 }
 
-func (s *ServiceSuite) TestGetStatistics() {
+func (s *MainSuite) TestGetStatistics() {
     response, err := core.GetStatistics("testdata")
 
     assert.Nil(s.T(), err)
+    assert.NotPanics(s.T(), func() {core.GetStatistics("testdata")}) //nolint:errcheck
 
-    for title, item := range response.Languages {
-        assert.Equal(s.T(), s.results[title]["LOC"], item.LOC)
-        assert.Equal(s.T(), s.results[title]["Files"], item.Files)
+    for title, item := range s.results.Components {
+        println(title)
+        assert.Equal(s.T(), item.LOC, response.Components[title].LOC)
+        assert.Equal(s.T(), item.Files, response.Components[title].Files)
     }
-
-    for title, item := range response.Components {
-        assert.Equal(s.T(), s.results[title]["LOC"], item.LOC)
-        assert.Equal(s.T(), s.results[title]["Files"], item.Files)
+    for title, item := range s.results.Languages{
+        assert.Equal(s.T(), item.LOC, response.Languages[title].LOC)
+        assert.Equal(s.T(), item.Files, response.Languages[title].Files)
     }
-
-    assert.Equal(s.T(), s.results["Total"]["LOC"], response.Total.LOC)
-    assert.Equal(s.T(), s.results["Total"]["Files"], response.Total.Files)
+    assert.Equal(s.T(), s.results.Total.LOC, response.Total.LOC)
+    assert.Equal(s.T(), s.results.Total.Files, response.Total.Files)
 }
 
-func TestServiceSuite(t *testing.T) {
-	suite.Run(t, new(ServiceSuite))
+func TestMainSuite(t *testing.T) {
+	suite.Run(t, new(MainSuite))
 }
